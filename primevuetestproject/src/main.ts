b1e918ca8aa5d1b752ccbc9558f106565fbf34c5
Travis Huffman <travis.huffman@wwinc.com>
2021-03-04 11:02:08 -0600

    Added vue router

--- primevuetestproject/src/main.ts
+++ primevuetestproject/src/main.ts
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Dialog from 'primevue/dialog';
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App).use(router)
+
+app.use(PrimeVue);
+app.component('Dialog', Dialog);
+
+app.mount('#app');
